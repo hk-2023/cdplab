7aaf0a067423777c0cb417d00f68e76fbf93021f
hk-2023 <harsh.kapoor.2788@gmail.com>
2023-02-13 20:33:15 +0530

    test

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 function openForm() {
   document.getElementById("myForm").style.display = "block";
+  document.querySelectorAll(".form-register").forEach(function(field){field.style.display='none'})
 }
 
 function closeForm() {
@@ -9,11 +10,23 @@ function closeForm() {
 function setAction(form) {
   
   var email = form.querySelector("#form-userid").value;
-  alert(email);
+  var fname = form.querySelector("#form-fname").value;
+  var lname = form.querySelector("#form-lname").value;
+
+
 analytics.identify( email,{
-  name:email,
-  email: email}
+  email: email,
+  firstName:fname,
+  lastName:lname}
 );
-/*form.action = "index.html";*/
+form.action = "index.html";
   return false;
+}
+
+function loginForm(form) {
+  document.querySelectorAll(".form-register").forEach(function(field){field.style.display='none'})
+
+}
+function registerForm(form) {
+  document.querySelectorAll(".form-register").forEach(function(field){field.style.display='block'})
 }
