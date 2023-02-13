aaf5102215781e16cc380cb9459253cbb3122d7d
hk-2023 <harsh.kapoor.2788@gmail.com>
2023-02-13 14:40:51 +0530

    udpates

--- js/login.js
+++ js/login.js
@@ -6,3 +6,14 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
+function setAction(form) {
+  
+  var email = form.querySelector("#form-userid").value;
+  alert(email);
+analytics.identify( email,{
+  name:email,
+  email: email}
+);
+/*form.action = "index.html";*/
+  return false;
+}
